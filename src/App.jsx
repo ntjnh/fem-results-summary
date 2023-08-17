5506f34decb8151870f28a1a943abbdc501bc3d3
Nate J.M <nate@natejonah.com>
2023-08-17 21:20:25 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+
+    return (
+        <>
+            <main>
+                <div>
+                    Your Result
+                    76
+                    of 100
+
+                    Great
+                    You scored higher than 65% of the people who have taken these tests.
+                    
+                    Summary
+
+                    Reaction
+                    80 / 100
+
+                    Memory
+                    92 / 100
+
+                    Verbal
+                    61 / 100
+
+                    Visual
+                    72 / 100
+
+                    Continue
+                </div>
+            </main>
+
+            <footer className="attribution">
+                Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank" rel="noreferrer">Frontend Mentor</a>. 
+                Coded by <a href="https://natejonah.com" target="_blank" rel="noreferrer">Nate</a>.
+            </footer>
+        </>
+    )
+}
+
+export default App
