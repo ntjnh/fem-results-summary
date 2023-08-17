9bfeac85a4a2f32defbb8d57fee6a49eb81caa6b
Nate J.M <nate@natejonah.com>
2023-08-17 21:20:26 +0100

    Finish styling Summary component on mobile

--- src/components/Summary.jsx
+++ src/components/Summary.jsx
@@ -5,22 +5,22 @@ export default function Summary() {
         <div className='summary'>
             <h3 className='title'>Summary</h3>
 
-            <div className='category light-red'>
+            <div className='category reaction'>
                 <h5 className='category__name'>Reaction</h5>
                 <h5 className='category__score'>80 <span className='out-of'>/ 100</span></h5>
             </div>
 
-            <div className='category orangey-yellow'>
+            <div className='category memory'>
                 <h5 className='category__name'>Memory</h5>
                 <h5 className='category__score'>92 <span className='out-of'>/ 100</span></h5>
             </div>
 
-            <div className='category green-teal'>
+            <div className='category verbal'>
                 <h5 className='category__name'>Verbal</h5>
                 <h5 className='category__score'>61 <span className='out-of'>/ 100</span></h5>
             </div>
 
-            <div className='category cobalt-blue'>
+            <div className='category visual'>
                 <h5 className='category__name'>Visual</h5>
                 <h5 className='category__score'>72 <span className='out-of'>/ 100</span></h5>
             </div>
