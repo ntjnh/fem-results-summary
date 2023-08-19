3fbd9bbfdba22aa65b72bea6ed6fb8856b662286
Nate J.M <nate@natejonah.com>
2023-08-19 21:11:52 +0100

    Import Summary data from JSON file

--- src/components/Summary.jsx
+++ src/components/Summary.jsx
@@ -1,29 +1,30 @@
 import './Summary.scss'
+import data from '../data.json';
+import memory from '../assets/images/icon-memory.svg'
+import reaction from '../assets/images/icon-reaction.svg'
+import verbal from '../assets/images/icon-verbal.svg'
+import visual from '../assets/images/icon-visual.svg'
+
+const icons = [reaction, memory, verbal, visual]
 
 export default function Summary() {
+
+    const categories = data.map((cat, i) => {
+        return (
+            <div key={i} className={`category ${cat.category.toLowerCase()}`}>
+                <span className='icon' style={{backgroundImage: `url(${icons[i]})`}}></span>
+
+                <h5 className='category__name'>{cat.category}</h5>
+                <h5 className='category__score'>{cat.score} <span className='out-of'>/ 100</span></h5>
+            </div>
+        )
+    })
+
     return (
         <div className='summary'>
             <h3 className='title'>Summary</h3>
 
-            <div className='category reaction'>
-                <h5 className='category__name'>Reaction</h5>
-                <h5 className='category__score'>80 <span className='out-of'>/ 100</span></h5>
-            </div>
-
-            <div className='category memory'>
-                <h5 className='category__name'>Memory</h5>
-                <h5 className='category__score'>92 <span className='out-of'>/ 100</span></h5>
-            </div>
-
-            <div className='category verbal'>
-                <h5 className='category__name'>Verbal</h5>
-                <h5 className='category__score'>61 <span className='out-of'>/ 100</span></h5>
-            </div>
-
-            <div className='category visual'>
-                <h5 className='category__name'>Visual</h5>
-                <h5 className='category__score'>72 <span className='out-of'>/ 100</span></h5>
-            </div>
+            {categories}
 
             <button className='summary__cta'>Continue</button>
         </div>
