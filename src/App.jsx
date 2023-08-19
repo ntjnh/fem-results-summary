e62824e56efec085d8e30476506ecc672f66e2ef
Nate J.M <nate@natejonah.com>
2023-08-19 21:12:38 +0100

    Add CSS classes to App component

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,18 @@ import Summary from './components/Summary'
 
 function App() {
     return (
-        <div>
-            <main>
-                <div>
+        <div className='App'>
+            <main className='container'>
+                <div className='card-container'>
                     <Results />
                     <Summary />
                 </div>
             </main>
 
-            <footer className="attribution">
+            <footer className='attribution'>
                 <div>
-                    Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank" rel="noreferrer">Frontend Mentor</a>.<br />
-                    Coded by <a href="https://natejonah.com" target="_blank" rel="noreferrer">Nate</a>.
+                    Challenge by <a href='https://www.frontendmentor.io?ref=challenge' target='_blank' rel='noreferrer'>Frontend Mentor</a>.<br />
+                    Coded by <a href='https://natejonah.com' target='_blank' rel='noreferrer'>Nate</a>.
                 </div>
             </footer>
         </div>
