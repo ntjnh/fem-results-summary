cf325cccb171ce942751ab52f8a819e98e81287c
Nate J.M <nate@natejonah.com>
2023-08-19 21:11:01 +0100

    Style Results component on desktop

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -3,14 +3,16 @@ import './Results.scss'
 export default function Results() {
     return (
         <div className='results'>
-            <h3 className='title'>Your Result</h3>
+            <div className="results-inner">
+                <h3 className='title'>Your Result</h3>
 
-            <div className='score-circle'>
-                <h1 className='score'>76<span className='score-out-of'>of 100</span></h1>
+                <div className='score-circle'>
+                    <h1 className='score'>76<span className='score-out-of'>of 100</span></h1>
+                </div>
+
+                <h4 className='feedback-title'>Great</h4>
+                <p className='feedback'>You scored higher than 65% of the people who have taken these tests.</p>
             </div>
-
-            <h4 className='feedback-title'>Great</h4>
-            <p className='feedback'>You scored higher than 65% of the people who have taken these tests.</p>
         </div>
     )
 }
